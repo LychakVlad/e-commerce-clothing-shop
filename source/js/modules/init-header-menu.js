98f8bb63f03dec971a4a9cb59df2d1f8ad1a2f54
Vlad <lycakvladislav@gmail.com>
2023-02-23 23:15:53 +0500

    fix header menu

--- source/js/modules/init-header-menu.js
+++ source/js/modules/init-header-menu.js
@@ -1,35 +1,52 @@
 const menuButton = document.querySelectorAll('.nav-menu-open');
-const headerMenu = document.querySelector('.header-menu');
 const burgerIcon = document.querySelector('.header-menu-open');
 const selectBody = document.querySelector('body');
-const headerLink = document.querySelector('header-nav__link');
+
+
 
 const initHeaderMenu = () => {
-  if (!headerMenu) {
+  if (!menuButton) {
     return;
   }
 
+  const handler = (evt) => {
+    const target = evt.target
+    const menuItem = evt.target.closest('.nav-menu-open');
+
+    const headerMenu = menuItem.lastElementChild
+    const headerLink = menuItem.firstElementChild
+    const headerLINK = document.querySelectorAll('.header-nav__link')
+
+    headerMenu.classList.toggle('is-active');
+
+    if (headerMenu.classList.contains('is-active')) {
+      headerLink.classList.add('is-active');
+      headerLINK.forEach((item) => {
+        item.classList.add('is-disabled')
+      });
+    } else {
+      headerLink.classList.remove('is-active');
+      headerLINK.forEach((item) => {
+        item.classList.remove('is-disabled')
+      });
+    }
+
+  }
+
+
+
   menuButton.forEach((item) => {
-    item.addEventListener('mouseenter', function (e) {
-      headerMenu.classList.toggle('is-active');
-    });
-
-    item.addEventListener('mouseleave', function (e) {
-      headerMenu.classList.toggle('is-active');
-    });
-
-    if (headerMenu.classList.contains('is-active') {
-      headerLink.classList.toggle('is-active');
-    })
-});
+    item.addEventListener('mouseenter', handler);
+    item.addEventListener('mouseleave', handler);
+  });
 
 
-burgerIcon.addEventListener('click', () => {
-  headerMenu.classList.toggle('is-active');
-  burgerIcon.classList.toggle('is-active');
-  selectBody.classList.toggle('scroll-lock');
 
-});
+  burgerIcon.addEventListener('click', () => {
+    burgerIcon.classList.toggle('is-active');
+    selectBody.classList.toggle('scroll-lock');
+
+  });
 };
 
 export { initHeaderMenu };
