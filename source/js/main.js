5506634c99827d834aef41afc38854e06c45a13e
Vlad <lycakvladislav@gmail.com>
2023-02-28 04:22:47 +0500

    fix js tabs

--- source/js/main.js
+++ source/js/main.js
@@ -26,16 +26,19 @@ window.addEventListener('DOMContentLoaded', () => {
     item.addEventListener('click', function (e) {
       e.preventDefault();
 
+      const id = e.target.getAttribute('href').replace('#', '')
+
       tabItem.forEach((child) => {
-        child.classList.remove('is-active')
-      })
+        child.classList.remove('is-active');
+      });
 
       tabContent.forEach((child) => {
-        child.classList.remove('is-active')
-      })
+        child.classList.remove('is-active');
 
-      item.classList.add('is-active')
-      
+      });
+
+      item.classList.add('is-active');
+      document.getElementById(id).classList.add('is-active');
     })
   })
 
