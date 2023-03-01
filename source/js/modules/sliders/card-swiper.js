27295177c304a5a385d5386aae3aa3f2e5ad1c2a
Vlad <lycakvladislav@gmail.com>
2023-03-01 11:51:54 +0500

    fix card page

--- source/js/modules/sliders/card-swiper.js
+++ source/js/modules/sliders/card-swiper.js
@@ -8,6 +8,19 @@ window.cardSlider = () => {
     loop: true,
     slidesPerView: 1,
     spaceBetween: 20,
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        centeredSlides: false,
+      },
+      560: {
+        slidesPerView: 2,
+        centeredSlides: true,
+      },
+      800: {
+        slidesPerView: 1,
+      },
+    },
     thumbs: {
       swiper: {
         el: '.card-slider-mini',
