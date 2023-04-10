1242bbbe64273c7119b4c428f757362fb7d99cb0
Vlad <lycakvladislav@gmail.com>
2023-04-09 20:59:41 -0400

    fix header menu

--- source/js/modules/init-header-menu.js
+++ source/js/modules/init-header-menu.js
@@ -4,36 +4,35 @@ const selectBody = document.querySelector('body');
 const headerMobMenu = document.querySelector('.header-menu-mob');
 
 
-
 const initHeaderMenu = () => {
   if (!menuButton) {
     return;
   }
 
   const handler = (evt) => {
-    const item = evt.target
+    const item = evt.target;
     const menuItem = item.closest('.nav-menu-open');
 
-    const headerMenu = menuItem.lastElementChild
-    const headerLink = menuItem.firstElementChild
+    const headerMenu = menuItem.lastElementChild;
+    const headerLink = menuItem.firstElementChild;
 
     headerMenu.classList.toggle('is-active');
 
     if (headerMenu.classList.contains('is-active')) {
       headerLink.classList.add('is-active');
+      selectBody.classList.add('scroll-lock');
     } else {
       headerLink.classList.remove('is-active');
+      selectBody.classList.remove('scroll-lock');
 
     }
-  }
-
+  };
 
 
   menuButton.forEach((item) => {
     item.addEventListener('mouseenter', handler);
     item.addEventListener('mouseleave', handler);
   });
-
 
 
   burgerIcon.addEventListener('click', () => {
